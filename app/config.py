98f65cd5f0c1d743076d6b32e551eb1ffc1166df
BereketZz <bereketzewde989@gmail.com>
2025-07-11 13:37:20 +0300

    task one telegram scrapping

---
+++ app/config.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_API_ID = int(os.getenv("TELEGRAM_API_ID"))
+TELEGRAM_API_HASH = os.getenv("TELEGRAM_API_HASH")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
